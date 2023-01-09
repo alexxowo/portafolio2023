f404f18cec8590d52f341fb8189600e36c3aaf25
Alexandre Marquez <alexandre.marquez@lebaocenter.com>
2023-01-09 18:18:25 -0400

    Added projects

--- app/components/projects/projects.jsx
+++ app/components/projects/projects.jsx
@@ -1,4 +1,22 @@
 import style from './project.module.css'
+import ProjectCard from './ProjectCard'
+
+import Placeholder from '../../../public/placeholder.png'
+
+const PROJECTS = [{
+  id: 1,
+  title: 'Alexandre Marquez Portfolio',
+  description: 'Portfolio website built with Next.js',
+  image: Placeholder,
+  technologies: ['Next.js', 'React']
+},
+{
+  id: 2,
+  title: 'Smart Traffic Lights',
+  description: 'Implementation of a smart traffic light using object detection with Firefly RK3566 and ESP32',
+  image: Placeholder,
+  technologies: ['Python', 'C++', 'Tensorflow', 'ESP32']
+}]
 
 export default function Projects () {
   return (
@@ -6,7 +24,15 @@ export default function Projects () {
       <div className={style.content}>
         <h2 className={style.title + ' heading-lg'}>Projects</h2>
         <div className={style.projects__container}>
-          <p>Projects empty (Work in Progress)</p>
+          {PROJECTS.map(project => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              technologies={project.technologies}
+            />
+          ))}
         </div>
       </div>
     </section>
