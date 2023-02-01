7ad0258a66652fc43ba620b1ed10424502acef7a
Alexandre Marquez <alexandre.marquez@lebaocenter.com>
2023-01-31 23:11:13 -0400

    Fixing some details

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -8,6 +8,13 @@ export default function Footer () {
       <div className={style.footer__container}>
         <div className={style.footer__header}>
           <h1 className={style.footer__header_title}><b>Alexandre</b> Marquez</h1>
+          <div className={style.animate__text_slider}>
+            <div className={style.animate_text}>
+              <div className={style.first}>Frontend Developer</div>
+              <div className={style.second}>Backend Developer</div>
+              <div className={style.third}>Sys Admin</div>
+            </div>
+          </div>
         </div>
         <div className={style.footer__content}>
           <ul className='navigator'>
@@ -23,21 +30,21 @@ export default function Footer () {
         <hr />
         <div className={style.footer__footer}>
           <div className={style.footer__footer__item}>
-            <p>Contacto</p>
+            <p>Contact me</p>
             <a href='#'>+58 412-6927900</a>
             <a href='mailto:alexandremarquez90@gmail.com'>alexandremarquez90@gmail.com</a>
           </div>
           <div className={style.footer__footer__item}>
-            <p>¿Buscas contratarme?</p>
-            <p>Escribeme a:</p>
+            <p>You want contract me?</p>
+            <p>Write me at:</p>
             <a href='mailto:alexandremarquez90@gmail.com'>alexandremarquez90@gmail.com</a>
           </div>
           <div className={style.footer__footer__item}>
-            <p>Sigue mis proyectos en mi github:</p>
+            <p>Follow me and my projects on github:</p>
             <a href='https://www.github.com/alexxowo'>github.com/alexxowo</a>
           </div>
           <div className={style.footer__footer__item}>
-            <p>¿Quieres saber más de mi?</p>
+            <p>You want know more about me?</p>
             <a href='https://www.linkedin.com/in/alexandre-manuel-m%C3%A1rquez-ortiz-8a1970169/'>linkedin.com/in/alexandremarquez90</a>
           </div>
         </div>
